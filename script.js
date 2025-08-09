42b68ed412bb3457891300d43383e8cfaa0d390d
lusuga404 <lusuga404@gmail.com>
2025-08-09 15:02:31 -0500

    se crea funcion final para mostrar la factura completa

--- script.js
+++ script.js
@@ -25,7 +25,45 @@ const calcularTotal = () => {
     const cargo = obtenerCargoFijo();
     return subtotal + iva + cargo;
 }
+// 8. Función final: mostrar la factura completa
+function mostrarFactura() {
 
+    const consumo = obtenerConsumo();
+    const tarifa = obtenerTarifa();
+    const cargoFijo = obtenerCargoFijo();
+    const porcentajeIVA = obtenerPorcentajeIVA();
+    const subtotal = calcularSubtotal();
+    const montoIVA = calcularIVA();
+    const total = calcularTotal();
+
+    console.log("\n   FACTURA DE ENERGÍA    ");
+    console.log("Consumo mensual:", consumo, "kilovatio");
+    console.log("Tarifa por kilovatio:", tarifa, "pesos");
+    console.log("Subtotal:", subtotal, "pesos");
+    console.log("Cargo fijo:", cargoFijo, "pesos");
+    console.log("Porcentaje IVA:", porcentajeIVA, "%");
+    console.log(" IVA:", montoIVA, "pesos");
+    console.log("--------------------------------");
+    console.log("TOTAL A PAGAR:", total, "pesos \n");
+}
+
+let continuar = true;
+while (continuar) {
+    consumoGuardado = 0;
+    tarifaGuardada = 0;
+    cargoFijoGuardado = 0;
+    porcentajeIVAGuardado = 0;
+
+    mostrarFactura();
+
+    const respuesta = prompt("¿Deseas generar otra factura? Escribe si 'no', cualquier otra tecla para continuar:");
+
+    if (respuesta === "no" || respuesta === "n") {
+        continuar = false;
+    }
+}
+
+console.log("Gracias por usar el sistema de facturación.");
 
 
 
